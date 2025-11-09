011175f33e6ed4be86d19f76fa6c7ab988868e69
Suresh Parmar <biswajeet@gmail.com>
2025-11-09 19:04:50 +0530

    add new changes

--- src/layout/Index.js
+++ src/layout/Index.js
@@ -1,10 +1,14 @@
 import Header from "./Header";
 import Footer from "./Footer";
-import { Outlet } from "react-router-dom";
-
+import { Outlet, useLocation } from "react-router-dom";
+import ReactGA from "react-ga4";
 
 const Layout = () => {
-
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA.initialize("G-32FG39X2S5"); // your GA tracking ID
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, []);
   return (
     <div className="app-layout">
       <div className="content">
